64bea9fc3e20e8d3ff70b25b69b4513b10a97981
Billy Vong <billy@sentry.io>
2017-10-19 12:01:00 -0700

    remove example code

--- src/sentry/static/sentry/app/views/settings/project/projectSettingsLayout.jsx
+++ src/sentry/static/sentry/app/views/settings/project/projectSettingsLayout.jsx
@@ -13,7 +13,7 @@ class ProjectSettingsLayout extends React.Component {
     let {orgId, projectId} = this.props.params;
 
     return (
-      <ProjectContext {...this.props.params}>
+      <ProjectContext {...this.props.params} orgId={orgId} projectId={projectId}>
         <SettingsLayout
           {...this.props}
           renderNavigation={() => <ProjectSettingsNavigation {...this.props} />}>
@@ -22,103 +22,10 @@ class ProjectSettingsLayout extends React.Component {
             setProjectNavSection: () => {}
           })}
 
-          {/*<Example />*/}
         </SettingsLayout>
       </ProjectContext>
     );
   }
 }
-
-// const Example = () => (
-  // <Box>
-    // <SettingsPanel>
-      // <SettingsPanelHeader>
-        // <SettingsPanelHeaderHeading>
-          // Project Details
-        // </SettingsPanelHeaderHeading>
-      // </SettingsPanelHeader>
-      // <SettingsPanelBody>
-        // <SettingsPanelItem
-          // label="Project name"
-          // required={true}
-          // type="text"
-          // placeholder="Enter a project name..."
-          // defaultValue="Freight"
-          // help=""
-        // />
-        // <SettingsPanelItem
-          // error={true}
-          // label="Short name"
-          // required={true}
-          // type="text"
-          // placeholder="Enter a short name..."
-          // defaultValue="freight"
-          // help="A unique ID used to identify this project."
-        // />
-        // <SettingsPanelItem
-          // label="Team"
-          // required={true}
-          // type="dropdown"
-          // placeholder="Choose a team..."
-          // defaultValue="freight"
-        // />
-        // <SettingsPanelItem
-          // label="Email subject prefix"
-          // required={true}
-          // type="text"
-          // placeholder="Enter a subject prefix..."
-          // defaultValue="[FRGHT]"
-        // />
-      // </SettingsPanelBody>
-    // </SettingsPanel>
-
-    // <SettingsPanel>
-      // <SettingsPanelHeader>
-        // <SettingsPanelHeaderHeading>Event Settings</SettingsPanelHeaderHeading>
-      // </SettingsPanelHeader>
-      // <SettingsPanelBody>
-        // <SettingsPanelItem
-          // label="Allow shared issues"
-          // required={true}
-          // type="switch"
-          // help="Enable sharing of limited details on issues to anonymous users."
-        // />
-        // <SettingsPanelItem
-          // label="Enhanced security"
-          // required={true}
-          // type="switch"
-          // help="Limits personally identifiable information (PII) and removes source code from alerts."
-        // />
-        // <SettingsPanelItem
-          // label="Global sensitive fields"
-          // placeholder="Add a field..."
-          // type="textarea"
-          // help="What does this do?"
-        // />
-        // <SettingsPanelItem
-          // label="Global safe fields"
-          // placeholder="Add a field..."
-          // type="textarea"
-          // help="What does this do?"
-        // />
-      // </SettingsPanelBody>
-    // </SettingsPanel>
-  // </Box>
-// );
-
-// const SettingsPanel = styled.div`
-  // background: #fff;
-  // border-radius: ${p => p.theme.radius};
-  // border: 1px solid ${p => p.theme.borderDark};
-  // box-shadow: ${p => p.theme.dropShadowLight};
-  // margin-bottom: 30px;
-// `;
-
-// const SettingsPanelHeader = styled.div`
-  // border-bottom: 1px solid ${p => p.theme.borderDark};
-  // border-radius: ${p => p.theme.radius} ${p => p.theme.radius} 0 0;
-  // background: ${p => p.theme.offWhite}
-  // padding: 15px 20px;
-// `;
 
 export default ProjectSettingsLayout;
