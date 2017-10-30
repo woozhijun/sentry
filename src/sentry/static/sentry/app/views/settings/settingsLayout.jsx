ed6425f5272b6d19bce5cecd642cb00fdd152396
Billy Vong <billy@sentry.io>
2017-10-30 15:43:29 -0700

    Add temp warning message for new settings pages

--- src/sentry/static/sentry/app/views/settings/settingsLayout.jsx
+++ src/sentry/static/sentry/app/views/settings/settingsLayout.jsx
@@ -1,4 +1,5 @@
 import {Box, Flex} from 'grid-emotion';
+import {Link} from 'react-router';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'react-emotion';
@@ -6,6 +7,33 @@ import styled from 'react-emotion';
 import SettingsActivity from './components/settingsActivity';
 import SettingsBreadcrumb from './components/settingsBreadcrumb';
 import SettingsHeader from './components/settingsHeader';
+
+let StyledWarning = styled.div`
+  margin-bottom: 30px;
+`;
+
+// TODO(billy): Temp
+let NewSettingsWarning = ({location = {}}) => {
+  // TODO(billy): Remove this warning when ready
+  let oldLocation = location.pathname
+    ? location.pathname.replace(/^\/settings\/organization\//, '/organizations/')
+    : '';
+  // members or auth should not be react routes
+  let isRouter = !/\/(members|auth)\//.test(location.pathname);
+
+  let linkProps = {
+    href: isRouter ? undefined : oldLocation,
+    to: isRouter ? oldLocation : undefined,
+  };
+  let Component = isRouter ? Link : 'a';
+
+  return (
+    <StyledWarning className="alert alert-warning">
+      These settings are currently in beta. Please report any issues. You can temporarily
+      visit the <Component {...linkProps}>old settings page</Component> if necessary.
+    </StyledWarning>
+  );
+};
 
 const Content = styled(Box)`
   flex: 1;
@@ -39,7 +67,11 @@ class SettingsLayout extends React.Component {
             </StickySidebar>
           </Box>
 
-          <Content>{children}</Content>
+          <Content>
+            <NewSettingsWarning location={this.props.location} />
+
+            {children}
+          </Content>
         </Flex>
       </div>
     );
